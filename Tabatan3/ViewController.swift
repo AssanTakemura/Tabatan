4b117fb29b155e4fcc6e659178f42f461c585336
Asuka Takemura <assan.takemura@gamil.com>
2020-11-27 19:23:23 +0900

    Initial Commit

---
+++ Tabatan3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tabatan3
+//
+//  Created by 竹村明日香 on 2020/11/27.
+//  Copyright © 2020 Takemura assan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tabatan3Tests/Tabatan3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Tabatan3Tests.swift
+//  Tabatan3Tests
+//
+//  Created by 竹村明日香 on 2020/11/27.
+//  Copyright © 2020 Takemura assan. All rights reserved.
+//
+
+import XCTest
+@testable import Tabatan3
+
+class Tabatan3Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
